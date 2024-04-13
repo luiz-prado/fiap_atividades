5ea2510a0190f21b3f5a5cf0e717f5caf6111d1a
luiz-prado <pradoluiz1502@gmail.com>
2024-04-13 19:55:33 -0300

    :construction: Criando algoritmo Python

--- Atividade - Fase 2/Arquivos/algoritmo_produto.py
+++ Atividade - Fase 2/Arquivos/algoritmo_produto.py
@@ -1,28 +1,47 @@
 import csv
 
 def cadastrar_produto():
-    produtos = []
-    while True:
-        descricao = input("Digite a descrição do produto: ")
-        valor = input("Digite o valor do produto: ")
-        embalagem = input("Digite o tipo de embalagem: ")
-        
-        produtos.append([descricao, valor, embalagem])
-        
-        resposta = input("Deseja cadastrar um novo produto? (sim/não): ")
-        if resposta.lower() != "sim":
-            break
+    produtos = {'descricao_produto': '', 'valor_produto': '', 'tipo_embalagem': ''}
+
+    try:
+        while True:
+            descricao = input("Digite a descrição do produto: ")
+            valor = float(input("Digite o valor do produto: "))
+            embalagem = input("Digite o tipo de embalagem: ")
+            
+            produtos['descricao_produto'] = descricao
+            produtos['valor_produto'] = valor
+            produtos['tipo_embalagem'] = embalagem
+            
+            resposta = input("Deseja cadastrar um novo produto? (sim/não): ")
+            if resposta.lower() != "sim":
+                break
+    except ValueError:
+        print("Valor inválido. Por favor, insira um valor numérico válido.")
+    except KeyboardInterrupt:
+        print("Operação interrompida pelo usuário.")
+    except Exception as erro:
+        print("Erro desconhecido:", erro)
+    finally:
+        print("Cadastro finalizado.")
     
     return produtos
 
+
 def criar_arquivo_csv(produtos):
     try:
-        with open("Arquivo 1_5_arquivo_produto.csv", "a", newline="") as arquivo:
-            writer = csv.writer(arquivo)
-            writer.writerows(produtos)
-        print("Arquivo CSV criado com sucesso!")
-    except Exception as e:
-        print("Erro ao criar o arquivo CSV:", str(e))
+        if len(produtos) < 5:
+            raise Exception("É necessário cadastrar no mínimo 5 produtos.")
+        else:
+            with open("Arquivo 1_5_arquivo_produto.csv", "a", newline="") as arquivo:
+                writer = csv.writer(arquivo)
+                writer.writerow(produtos.keys())
+                
+                for produto in produtos:
+                    writer.writerow([val[produto] for val in produto.values()])
+            print("Arquivo CSV criado com sucesso!")
+    except Exception as erro:
+        print("Não foi possivel carregar as informações:", erro)
 
 produtos = cadastrar_produto()
 criar_arquivo_csv(produtos)
--- Atividade - Fase 2/Arquivos/teste.py
+++ Atividade - Fase 2/Arquivos/teste.py
@@ -1,35 +1,17 @@
 import csv
 
-def cadastrar_produto():
-    produtos = []
-    while True:
-        descricao = input("Digite a descrição do produto: ")
-        valor = input("Digite o valor do produto: ")
-        embalagem = input("Digite o tipo de embalagem: ")
-        
-        produtos.append([descricao, valor, embalagem])
-        
-        resposta = input("Deseja cadastrar um novo produto? (sim/não): ")
-        if resposta.lower() != "sim":
-            break
-    
-    return produtos
-
-def criar_arquivo_csv(produtos):
-    nome_arquivo = "1_5_arquivo_produto.csv"
-    with open(nome_arquivo, "a", newline="") as arquivo:
-        writer = csv.writer(arquivo)
-        writer.writerow(["Descricao", "Valor", "Embalagem"])
-        writer.writerows(produtos)
-    
-    print(f"Arquivo {nome_arquivo} criado com sucesso!")
+produtos = {'Descricao': '', 'Valor': '', 'Tipo de embalagem': ''}
 
 try:
-    produtos = cadastrar_produto()
-    if len(produtos) >= 5:
-        criar_arquivo_csv(produtos)
-    else:
-        print("É necessário cadastrar no mínimo 5 produtos.")
-        produtos = cadastrar_produto()
-except Exception as e:
-    print(f"Ocorreu um erro: {str(e)}")
+    while True:
+        descricao = input('Digite a descrição do produto:')
+        valor = float(input('Digite o valor do produto:'))
+        tipo_embalagem = input('Digite o tipo de embalagem do produto:')
+except ValueError:
+    print('Valor inválido. Por favor, insira um valor numérico válido.')
+except KeyboardInterrupt:
+    print('Operação interrompida pelo usuário.')
+except Exception as erro:
+    print('Erro desconhecido:', erro)
+finally:
+    print('Cadastro finalizado.')
