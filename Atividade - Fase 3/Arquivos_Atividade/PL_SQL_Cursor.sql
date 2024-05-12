492c6bdc90bcfc82be416c7de5f00cb1b858f7c6
luiz-prado <pradoluiz1502@gmail.com>
2024-05-11 21:52:36 -0300

    :memo: Adicionando arquivos da atividade da fase 3

---
+++ Atividade - Fase 3/Arquivos_Atividade/PL_SQL_Cursor.sql
@@ -0,0 +1,114 @@
+DECLARE
+    -- Item a) Criação do cursor
+    CURSOR sac_cursor IS
+        SELECT
+            s.nr_sac AS numero_ocorrencia_sac,
+            s.dt_abertura_sac AS data_abertura_sac,
+            s.hr_abertura_sac AS hora_abertura_sac,
+            s.tp_sac AS tipo_sac,
+            p.cd_produto AS codigo_prod,
+            p.ds_produto AS nome_prod,
+            p.vl_unitario AS vl_unitario_prod,
+            p.vl_perc_lucro AS percentual_lucro_prod,
+            c.nr_cliente AS numero_cliente,
+            c.nm_cliente AS nome_cliente
+        FROM
+            mc_sgv_sac s
+            JOIN mc_produto p ON s.cd_produto = p.cd_produto
+            JOIN mc_cliente c ON s.nr_cliente = c.nr_cliente;
+        
+    v_tipo_sac VARCHAR(25);
+    v_unitario_lucro_prod mc_produto.vl_unitario%TYPE;
+    v_sg_estado mc_estado.sg_estado%TYPE;
+    v_nm_estado mc_estado.nm_estado%TYPE;
+    v_aliquota_icms mc_aliquota_media_icms_estado.vl_perc_aliquota_media%TYPE;
+    v_icms_produto mc_sgv_ocorrencia_sac.vl_icms_produto%TYPE;
+    v_cursor sac_cursor%rowtype;
+    
+BEGIN
+    IF NOT sac_cursor%ISOPEN THEN
+        OPEN sac_cursor;
+    END IF;
+    
+    LOOP
+        FETCH sac_cursor INTO v_cursor;
+        EXIT WHEN sac_cursor%NOTFOUND;
+        
+        -- Item b.1) Classificação dos Tipos de SAC
+        CASE
+            WHEN v_cursor.tipo_sac = 'S' THEN v_tipo_sac := 'SUGESTÃO';
+            WHEN v_cursor.tipo_sac = 'D' THEN v_tipo_sac := 'DÚVIDA';
+            WHEN v_cursor.tipo_sac = 'E' THEN v_tipo_sac := 'ELOGIO';
+            ELSE v_tipo_sac := 'CLASSIFICAÇÃO INVÁLIDA';
+        END CASE;
+        
+        -- Item b.2) Calculando o valor do lucro unitário do produto
+        v_unitario_lucro_prod := (v_cursor.percentual_lucro_prod/100)*v_cursor.vl_unitario_prod;
+        
+        -- Item b.3) Recuperando as informações SG_ESTADO e NM_ESTADO
+        SELECT
+            est.sg_estado,
+            est.nm_estado
+        INTO
+            v_sg_estado,
+            v_nm_estado
+        FROM
+            mc_end_cli e
+            JOIN mc_logradouro l ON e.cd_logradouro_cli = l.cd_logradouro
+            JOIN mc_bairro b ON l.cd_bairro = b.cd_bairro
+            JOIN mc_cidade cid ON b.cd_cidade = cid.cd_cidade
+            JOIN mc_estado est ON cid.sg_estado = est.sg_estado
+        WHERE
+            e.nr_cliente = v_cursor.numero_cliente;
+        
+        -- Item b.4) Recupenrando e calculando o valor médio do ICMS do produto
+        SELECT 
+            fun_mc_gera_aliquota_media_icms_estado(v_sg_estado)
+        INTO
+            v_aliquota_icms
+        FROM
+            dual;
+            
+        v_icms_produto := (v_aliquota_icms/100)*v_cursor.vl_unitario_prod;
+        
+        
+        -- Inserindo os dados na tabela MC_SGV_OCORRENCIA_SAC
+        INSERT INTO mc_sgv_ocorrencia_sac (
+            nr_ocorrencia_sac,
+            dt_abertura_sac,
+            hr_abertura_sac,
+            ds_tipo_classificacao_sac,
+            cd_produto,
+            ds_produto,
+            vl_unitario_produto,
+            vl_perc_lucro,
+            vl_unitario_lucro_produto,
+            sg_estado,
+            nm_estado,
+            nr_cliente,
+            nm_cliente,
+            vl_icms_produto)
+        VALUES (
+            v_cursor.numero_ocorrencia_sac,
+            v_cursor.data_abertura_sac,
+            v_cursor.hora_abertura_sac,
+            v_tipo_sac,
+            v_cursor.codigo_prod,
+            v_cursor.nome_prod,
+            v_cursor.vl_unitario_prod,
+            v_cursor.percentual_lucro_prod,
+            v_unitario_lucro_prod,
+            v_sg_estado,
+            v_nm_estado,
+            v_cursor.numero_cliente,
+            v_cursor.nome_cliente,
+            v_icms_produto
+        );
+
+        DBMS_OUTPUT.PUT_LINE('Dados inseridos na tabela MC_SGV_OCORRENCIA_SAC para o SAC número: ' || v_cursor.numero_ocorrencia_sac);
+    END LOOP;
+    
+    -- Encerrando o cursor
+    CLOSE sac_cursor;
+END;
+/
---
+++ Atividade - Fase 3/README.md
@@ -0,0 +1,33 @@
+# Fase-3 - Atividade PL/SQL
+
+Este arquivo README fornece uma descrição da atividade desenvolvida em PL/SQL.
+
+## Descrição
+
+Nesta atividade, focamos em aprender e praticar PL/SQL, que é uma extensão da linguagem SQL. PL/SQL nos permite escrever programas poderosos e eficientes para bancos de dados, incluindo stored procedures, funções, triggers e packages.
+
+## Objetivos
+
+Os principais objetivos desta atividade foram:
+
+1. Compreender a sintaxe básica e a estrutura do PL/SQL.
+2. Aprender como criar e executar blocos de PL/SQL.
+3. Praticar o uso de variáveis, loops, estruturas condicionais e cursores em PL/SQL.
+4. Explorar as capacidades do PL/SQL para programação de bancos de dados.
+
+## Conteúdo
+
+A atividade abordou os seguintes tópicos:
+
+1. Introdução ao PL/SQL
+2. Blocos e Execução de PL/SQL
+3. Variáveis e Tipos de Dados em PL/SQL
+4. Estruturas de Controle em PL/SQL (Loops e Estruturas Condicionais)
+5. Tratamento de Exceções em PL/SQL
+6. Cursores em PL/SQL
+
+## Recursos
+
+Durante a atividade, utilizamos os seguintes recursos:
+
+- SQL Developer: Utilizamos o SQL Developer como IDE para escrever e executar código PL/SQL.
